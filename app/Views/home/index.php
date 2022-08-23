6f608ecf447680674e18e00e083fa4df41f4bc1e
John Michael <withyou746@gmail.com>
2022-08-23 15:11:54 +0900

    content page simplize

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,3 +1,11 @@
 <div class="contentbar">
-    Hello world!
+    <div class="row">
+        <!-- Start col -->
+        <div class="col-md-12 col-lg-12 col-xl-12">
+            <div class="text-center mt-3 mb-5">
+                <h4>Page Title</h4>
+            </div>
+        </div>
+        <!-- End col -->
+    </div>
 </div>
